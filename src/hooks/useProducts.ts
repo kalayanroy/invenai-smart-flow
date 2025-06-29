1b3b4e325446cad320ae1d5d60c6099ad72673ab
kalayan chowdhury <46247135+kalayanroy@users.noreply.github.com>
2025-06-30 00:51:54 +0600

    useProducts_update

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -23,7 +23,7 @@ export interface Product {
 
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
-const PAGE_SIZE = 1;
+const PAGE_SIZE = 10;
   // Load products from Supabase on mount
   useEffect(() => {
     fetchProducts();
